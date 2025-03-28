f1105bfbd225956d054ecd7e1ceb1024603121ba
Arsen224114 <ayarmalitski@gmail.com>
2025-03-28 19:31:59 +0300

    tas03 update

--- Task03/logic.cpp
+++ Task03/logic.cpp
@@ -7,6 +7,50 @@
 
 #include "logic.h"
 
+// 0  1  2  3 4
+// 45 7 90 34 1
+
+int get_max(int* array, int size) {
+	int value = array[0];
+
+	for (int i = 0; i < size; i++)
+	{
+		if (array[i] > value) {
+			value = array[i];
+		}
+	}
+
+	return value;
+}
+
+int get_min(int* array, int size) {
+	int value = array[0];
+
+	for (int i = 0; i < size; i++)
+	{
+		if (array[i] < value) {
+			value = array[i];
+		}
+	}
+
+	return value;
+}
+
+
 double calculate_arithmetical_mean_of_nonextreme_elements(int* array, int size) {
-	return 0.0;
+	int max = get_max(array, size);
+	int min = get_min(array, size);
+
+	double sum = 0;
+	int count = 0;
+
+	for (int i = 0; i < size; i++)
+	{
+		if (array[i] != max && array[i] != min) {
+			sum += array[i];
+			count++;
+		}
+	}
+
+	return count > 0 ? sum / count : 0.0;
 }
