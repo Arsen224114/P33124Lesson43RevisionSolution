b9a62955153c6bfe853722cf1d5d1eb4dbd71fb7
Arsen224114 <ayarmalitski@gmail.com>
2025-03-28 19:56:32 +0300

    /

--- Task04/logic.cpp
+++ Task04/logic.cpp
@@ -6,6 +6,37 @@
 
 #include "logic.h"
 
+int get_max(int* array, int size) {
+	int value = array[0];
+
+	for (int i = 0; i < size; i++)
+	{
+		if (array[i] > value) {
+			value = array[i];
+		}
+	}
+
+	return value;
+}
+
+int get_min(int* array, int size) {
+	int value = array[0];
+
+	for (int i = 0; i < size; i++)
+	{
+		if (array[i] < value) {
+			value = array[i];
+		}
+	}
+
+	return value;
+}
+
 int sum_between_first_min_max(int* array, int size) {
+	int max = get_max(array, size);
+	int min = get_min(array, size);
+
+
+
 	return 0;
 }
